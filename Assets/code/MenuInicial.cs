70a351d012517a7b0ea618096fbd610b9c44fe31
Juan Carlos Carrera <carrera1995@hotmail.es>
2019-06-26 21:35:21 -0500

    second commit

---
+++ Assets/code/MenuInicial.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+
+
+    public void PlayGame()
+    {
+        Debug.Log("NEXT SCENE");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/code/goshtClass.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class goshtClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            gameObject.transform.Translate(-100f * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.Translate(100f * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            gameObject.transform.Translate(0, 100f * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("down"))
+        {
+            gameObject.transform.Translate(0, -100f * Time.deltaTime, 0);
+        }
+
+              
+
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "fantasmita")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+
+}
