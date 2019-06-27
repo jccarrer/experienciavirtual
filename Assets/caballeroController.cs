c467da5d8e3f8c90e23fa1073eb3acc7cd50c7ab
Juan Carlos Carrera <carrera1995@hotmail.es>
2019-06-26 23:24:12 -0500

    commit in produccion

---
+++ Assets/caballeroController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caballeroController : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crounch = false;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    
+    //}
+    //
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+        if (Input.GetKey("left"))
+        {
+            gameObject.transform.Translate(-10f * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.Translate(10f * Time.deltaTime, 0, 0);
+            //transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey("up"))
+        {
+            jump = true;
+            transform.Translate(Vector3.up * 11 * Time.deltaTime, Space.World);
+            //gameObject.transform.Translate(0, 10f * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("down"))
+        {
+            gameObject.transform.Translate(0, -10f * Time.deltaTime, 0);
+        }
+    }
+}
