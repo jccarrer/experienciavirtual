2d1d472d30ab09294151605f0e6ad9be66d80d4f
Juan Carlos Carrera <carrera1995@hotmail.es>
2019-07-22 06:13:19 -0500

    next level

---
+++ Assets/code/CamaraQueSigue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CamaraQueSigue : MonoBehaviour
+{
+
+    public Transform objetivo;
+    public  float  suavizado = 5f;
+    
+    Vector3 desface;
+    // Start is called before the first frame update
+    void Start()
+    {
+        desface= transform.position - objetivo.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 posicionObjetivo = objetivo.position + desface;
+        transform.position = Vector3.Lerp(transform.position,posicionObjetivo,suavizado*Time.deltaTime);
+        
+        
+    }
+}
